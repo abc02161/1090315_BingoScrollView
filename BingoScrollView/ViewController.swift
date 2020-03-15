071e13ad4f9ff7a976344d8898f8a166316a951b
Nick <abc02161@yahoo.com.tw>
2020-03-15 21:36:36 +0800

    Initial Commit

---
+++ BingoScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BingoScrollView
+//
+//  Created by Nick on 2020/3/15.
+//  Copyright © 2020 NewIdea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
